ce4d870bce24b65672b454bb4aa1f33426ed5ae6
Singam Snehalatha <145570483+Singam2204@users.noreply.github.com>
2023-10-11 18:28:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+
+// These symbols are used to insert comments in js
+
+// to output something in console page
+
+console.log("Hello World");
+
+// variables - the containers to store some values
+
+let ageofaperson = 35;
+
+let ageOfAPerson = 21; //camel case
+
+let age_of_a_person = 50;
+
+// the above 3 are different cases
+
+let nameofperson = "Singam"; //String
+
+let gradeofastudent = 'A'; //character
+
+console.log(ageOfAPerson);
+
+console.log(nameofperson);
+
+console.log(gradeofastudent);
+
+//constants - the value which doesn't changes in future cases
+
+const age_to_vote = 18;
+
+//age_to_vote = 22; //error
+
+console.log(age_to_vote);
+
+// operators
+
+let var1 = 4;
+
+let var2 = 5;
+
+// Arithmetic operators
+
+console.log(var1 + var2);
+
+console.log(var1 - var2);
+
+console.log(var1 * var2);
+
+console.log(var1 / var2);
+
+console.log(var1 % var2);
+
+console.log(var1 ** var2);
+
+var1++;
+
+console.log(var1);
+
+var2--;
+
+console.log(var2);
+
+// Assignment Operators
+
+let num = 22;
+
+num+= 5; //num=num+5
+
+console.log(num);
+
+num-= 5; //num=num-5
+
+console.log(num);
+
+num*= 2;
+
+console.log(num);
+
+num/= 2;
+
+console.log(num);
+
+// Comaprison Operators
+
+let num1 = 100;
+
+let num2 = 200;
+
+console.log(num1 == num2); //false
+
+console.log(num != num2); //true
+
+console.log(num1 >= num2); //false
+
+console.log(num1 <= num2); //true
+
+let number = 55; //number
+
+let value = "55"; //string
+
+console.log(number === value); //true
+
